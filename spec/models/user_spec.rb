f748b9998c5917bc85df4a48b929be9ca1a47d78
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-22 10:49:03 -0600

    User model and spec created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:email)}
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:password_confirmation)}
+
+  end
+
   it "exists" do
     user = User.new
     expect(user).to be_an_instance_of(User)

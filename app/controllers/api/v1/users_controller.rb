9da666b0b69b870c1e7b6e2c293b8cb08c01594a
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-22 13:43:22 -0600

    Add user show action to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,5 +5,10 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: UserSerializer.new(users)
   end
-  
+
+  # GET /api/v1/users/:id
+  def show
+    user = User.find(params[:id])
+    render json: UserSerializer.new(user)
+  end
 end

09c2660438cf6d3dfaffec4524c6a8941b12962b
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-28 14:13:23 -0600

    start test for repo create

--- spec/requests/api/v1/repos_request_spec.rb
+++ spec/requests/api/v1/repos_request_spec.rb
@@ -19,4 +19,34 @@ RSpec.describe "Api::V1::Repos", type: :request do
       expect(response.body).to include("86.05")
     end
   end
+
+  describe "Repos Create" do
+    it "creates a repo" do
+      repo_params = {
+        owner: "delaneymiranda1",
+        name: "be_cheap_date"
+      }
+
+      post api_v1_user_repo_path(@user, repo_params), params: repo_params
+
+      new_repo = Repo.last
+
+      expect(response).to be_successful
+      
+      expect(new_repo.owner).to eq("s2an")
+      expect(new_repo.name).to eq("lunch_and_learn_be_7")
+    end
+
+    xit 'sad path- if a user does not enter file' do
+
+      repo1 = @user.repos.create!(owner: "s2an", name: "tea_subscription_be_7")
+      post api_v1_user_repo_path(@user, repo1), params: repo1
+
+
+      expect(response).to_not be_successful
+      
+      expect(response).to eq(404)
+      expect(response.message).to eq("Not Found")
+    end
+  end
 end

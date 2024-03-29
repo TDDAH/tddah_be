75a8b79efe1de1454d36d4723504c1f71678309e
Miranda Delaney <delaney.miranda1@gmail.com>
2024-03-29 14:58:40 -0600

    Add achievement serializer and index route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :destroy] do
         resources :repos, only: [:index, :show, :create]
+        resources :achievements, only: [:index]
       end        
     end
   end

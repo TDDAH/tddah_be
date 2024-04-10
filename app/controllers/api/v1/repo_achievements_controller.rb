be6db81a7751eddd22abbf84591a04fd1915f2e3
Miranda Delaney <delaney.miranda1@gmail.com>
2024-04-10 15:46:36 -0600

    repo achievements being stored

--- app/controllers/api/v1/repo_achievements_controller.rb
+++ app/controllers/api/v1/repo_achievements_controller.rb
@@ -1,26 +1,6 @@
-class Api::V1::Users::Repos::RepoAchievementsController < ApplicationController
+class Api::V1::RepoAchievementsController < ApplicationController
   
-  # GET /api/v1/users/:user_id/repos/:id/achievements
-  def index
-    
-    # achievements = Achievements.all
-    # render json: AchievementSerializer.new(achievements)
-  end
+  # GET /api/v1/users/:user_id/repos/:id/achievements (storing for a repos achievements)
+  def index; end
 
-  # POST /api/v1/users/:user_id/repos/:id/achievements
-  def create
-    # achievement = Achievement.new(achievement_params)
-
-    # if achievement.save
-    #   render json: achievement, status: :created
-    # else
-    #   render json: achievement.errors, status: :unauthorized
-    # end
-  end
-
-  private
-
-  # def achievement_params
-  #   params.permit(:name, :criteria)
-  # end
 end
